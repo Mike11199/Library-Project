52ecffb28606bdd199dddeef8933941ce30ccc2a
Michael <miwanek11199@gmail.com>
2022-02-11 18:40:07 -0800

    added e.prevent default to be able to grab variables from the form and not submit it, then add to a new object of Book

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 const form = document.getElementById("myForm")
 const addButton = document.getElementById("addBookBtnForm")
+const AddButton =  document.getElementById("addBookbtn")
+const loginForm = document.getElementById("bookForm");
+const cancelBookBtnForm = document.getElementById("cancelBookBtnForm");
+const bookTitle = document.getElementById('bookTitle');
+const authorName = document.getElementById('authorName');
+const numPages = document.getElementById('numPages');
+
 
 let myLibrary = [];
 
-function Book(bookTitle) {
 
-    this.bookTitle = bookTitle;
 
+loginForm.addEventListener("submit", addBooktoLibrary);
+AddButton.addEventListener("click", ToggleForm);
+cancelBookBtnForm.addEventListener("click",closeForm);
+
+
+
+function Book(title, author, pages) {
+    this.bookTitle = title;
+    this.bookAuthor = author;
+    this.bookPages = pages;
 }
 
 
-function addBooktoLibrary () {
-    // add something here
+
+
+//This section gets all the inputs from the form entered by the user, and returns an object from the 'Book Class'.  
+//What's entered into the form is passed into the object and can be accessed as its properties.
+function addBooktoLibrary(e) {
+    e.preventDefault();
+    const newBook = new Book(bookTitle.value, authorName.value, numPages.value);
+    closeForm();
+    bookTitle.value="";
+    authorName.value="";
+    numPages.value="";
+    myLibrary.push(newBook);
+    alert(myLibrary[0].bookPages);
 }
 
-const Book1 = new Book('The Odyssey');
-console.log(Book1.bookTitle);
 
 
 
 
+//This lets the add book button be able to open and close the form depending on its state
 function ToggleForm() {
         
     if (form.style.display == "block") {
@@ -31,11 +56,14 @@ function ToggleForm() {
         form.style.boxShadow = "0px 0px 1px 5000px rgba(0,0,0,0.4)";
       
     }
-    
-
-        
-  }
+    }
   
+
+
+//Closes the form and resets everything
   function closeForm() {
         form.style.display = "none";
+        bookTitle.value="";
+        authorName.value="";
+        numPages.value="";
   }
