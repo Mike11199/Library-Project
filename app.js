a02072fa659677187433b9f7b3e80d90d6f66cef
Michael <miwanek11199@gmail.com>
2022-02-12 19:27:50 -0800

    added capability to link image URL, added function to add divs to libraryCard

--- app.js
+++ app.js
@@ -2,10 +2,16 @@ const form = document.getElementById("myForm")
 const AddCard =  document.getElementById("add_card")
 const loginForm = document.getElementById("bookForm");
 const cancelBookBtnForm = document.getElementById("cancelBookBtnForm");
+
 const bookTitle = document.getElementById('bookTitle');
 const authorName = document.getElementById('authorName');
 const numPages = document.getElementById('numPages');
+const bookCover = document.getElementById('bookCover');
+const isReadChecked = document.getElementById('isRead');
+let isRead = isReadChecked.checked;
+
 let libraryCount=0;
+
 
 let myLibrary = [];
 
@@ -17,10 +23,14 @@ cancelBookBtnForm.addEventListener("click",closeForm);
 
 
 
-function Book(title, author, pages) {
+
+//This is the javascript constructor for the book object.  bookCover = 0 makes it optional
+function Book(title, author, pages, isRead, bookCover = 0) {
     this.bookTitle = title;
     this.bookAuthor = author;
     this.bookPages = pages;
+    this.isRead = isRead;
+    this.bookCover = bookCover;
 }
 
 
@@ -30,12 +40,15 @@ function Book(title, author, pages) {
 //What's entered into the form is passed into the object and can be accessed as its properties.
 function addBooktoLibrary(e) {
     e.preventDefault();
-    const newBook = new Book(bookTitle.value, authorName.value, numPages.value);
+    //adds variables from form to the book constructor
+    const newBook = new Book(bookTitle.value, authorName.value, numPages.value, isRead.value, bookCover.value);
     closeForm();
     bookTitle.value="";
     authorName.value="";
     numPages.value="";
+    //add new book object entered in form to myLibrary array of objects
     myLibrary.push(newBook);
+    //Adds everything in the library to the grid
     addLibrarytoGrid();
 }
 
@@ -43,21 +56,65 @@ function addBooktoLibrary(e) {
 
 
 
-//This will take the object created by the addBooktoLibrary function and add it to card in the page grid.  It adds to the previous div, the AddCard div.
+//This will take all objects in the myLibrary array and add it to cards in the page grid.  It adds to the previous div, the AddCard div.
 function addLibrarytoGrid () {
 
-    let card = document.createElement("div");
-    let cardDetails = document.createElement("div");
-    card.id += `bookNum_${libraryCount}`;
-    card.className += `bookPane`;
-    AddCard.before(card);
-    card.appendChild(cardDetails)
-    cardDetails.innerHTML += "<p>" +myLibrary[libraryCount].bookTitle + "</p>"
-    cardDetails.innerHTML += "<p>" +myLibrary[libraryCount].bookAuthor + "</p>"
-    cardDetails.innerHTML += "<p>" +myLibrary[libraryCount].bookPages + "</p>"
-    libraryCount++;
+    //Loop through array of book objects in the library array --  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
+    // for 'const' book used instead of 'let as 'let' would require book++ at end of the 'for' statement
+    for (let i = 0, l = myLibrary.length; i < l; i++)
+    {
+    AddCard.before(createLibraryCard(myLibrary[i]));
+    }
 
 }
+
+
+const createLibraryCard = (book) => {
+
+    // This sets up all of the nodes or elements for the card that stores book info.  It does not add them to the DOM yet.
+    let libraryCard = document.createElement("div");
+    let bookCover = document.createElement("img");
+    let bookTitle = document.createElement("p");
+    let bookAuthor = document.createElement("p");
+    let bookPages = document.createElement("p");
+    let readButton = document.createElement("button");
+    let deleteButton = document.createElement("button");
+
+    //this adds classes to each of the elements created above so that CSS can be applied to them.
+    libraryCard.classList.add("libraryCard");
+    bookCover.classList.add("bookCover");
+    bookTitle.classList.add("bookTitle");
+    bookAuthor.classList.add("bookAuthor");
+    bookPages.classList.add("bookPages");
+    readButton.classList.add("readButton");
+    deleteButton.classList.add("deleteButton");
+
+
+    //This take the book object passed into this function and assigns its properties (title, author, etc.) to the divs
+    //and elements created for the library card.
+    bookTitle.textContent = book.bookTitle;
+    bookAuthor.textContent = book.bookAuthor;
+    bookPages.textContent = book.bookPages;
+    bookCover.src= book.bookCover;
+
+
+   //This uses .appendChild to add all the elements to the main div libraryCard.  This is the Div added to the grid. 
+    libraryCard.appendChild(bookCover)
+    
+    bookCover.appendChild(bookTitle)
+    bookCover.appendChild(bookAuthor)
+    bookCover.appendChild(bookPages)
+
+    bookCover.appendChild(readButton)
+    bookCover.appendChild(deleteButton)
+
+    return libraryCard;
+
+}
+
+
+
+
 
 
 
